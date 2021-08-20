3b88917a9e5d4c54ce5993536179d61fd6d0367d
y-chan <y-chan@y-chan.dev>
2021-08-20 10:42:04 +0900

    add metan and zundamon command

---
+++ bot_commands/metan.py
@@ -0,0 +1,13 @@
+from discord.ext import commands
+from discord.ext.commands.context import Context
+
+from bot_lib.common import TTSStartCommon
+
+
+class Metan(TTSStartCommon):
+    def __init__(self, bot):
+        super().__init__(bot)
+
+    @commands.command()
+    async def metan(self, ctx: Context):
+        await self.tts_setup(ctx, 0)
---
+++ bot_commands/zundamon.py
@@ -0,0 +1,13 @@
+from discord.ext import commands
+from discord.ext.commands.context import Context
+
+from bot_lib.common import TTSStartCommon
+
+
+class Zundamon(TTSStartCommon):
+    def __init__(self, bot):
+        super().__init__(bot)
+
+    @commands.command()
+    async def zundamon(self, ctx: Context):
+        await self.tts_setup(ctx, 1)

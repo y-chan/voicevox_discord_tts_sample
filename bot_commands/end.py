9851b73a384ed11c16de348d582f8b6e048ad4af
y-chan <y-chan@y-chan.dev>
2021-08-20 10:42:19 +0900

    add end command

---
+++ bot_commands/end.py
@@ -0,0 +1,22 @@
+from discord.ext import commands
+from discord.ext.commands.context import Context
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from run import TTSBotSample
+
+
+class End(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot  # type: TTSBotSample
+
+    @commands.command()
+    async def end(self, ctx: Context):
+        if ctx.guild is None:
+            await ctx.channel.send("サーバー内専用コマンドです。")
+            return
+        connection = self.bot.connections.get(ctx.guild.id)
+        if connection is None:
+            await ctx.channel.send(f"{self.bot.user.name}は現在TTS機能が起動していません。")
+            return
+        await self.bot.text_to_speech(connection, "TTS機能を終了します。", tts_end=True)
